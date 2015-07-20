b4ce3d205030202182918c8c22ff2731f8ab464e
ryan_pixel <icyprian8@gmail.com>
2015-07-20 18:28:35 +0300

    Fix for mobile editing issue

---
+++ html/js/mindmup-editabletable.js
@@ -0,0 +1,162 @@
+/*global $, window*/
+$.fn.editableTableWidget = function (options) {
+	'use strict';
+	return $(this).each(function () {
+		var buildDefaultOptions = function () {
+				var opts = $.extend({}, $.fn.editableTableWidget.defaultOptions);
+				opts.editor = opts.editor.clone();
+				return opts;
+			},
+			activeOptions = $.extend(buildDefaultOptions(), options),
+			ARROW_LEFT = 37, ARROW_UP = 38, ARROW_RIGHT = 39, ARROW_DOWN = 40, ENTER = 13, ESC = 27, TAB = 9,
+			element = $(this),
+			editor = activeOptions.editor.css('position', 'absolute').hide().appendTo(element.parent()),
+			active,
+			append,
+			showEditor = function (select) {
+				active = element.find('td:focus');
+				append = 0;
+				if(active.html().slice(1,2) == 'i'){
+					active = element.find('td:focus').prev('td');
+					append = 1;
+				}
+				// console.log("active is");
+				// console.log();
+				if (active.length && active.attr('data-function')=="editable") {
+					editor.val(parseInt(active.text().replace(/[^0-9\.]+/g,"")))
+						.removeClass('error')
+						.show()
+						.offset(active.offset())
+						.width(active.width()+16)
+						.height(active.height()+12)
+						.css(active.css(activeOptions.cloneProperties))
+						.focus();
+					if (select) {
+						editor.select();
+					}
+				}
+			},
+			setActiveText = function () {
+				var text = editor.val(),
+					evt = $.Event('change'),
+					originalContent;
+					console.log("Changing for:"+active.html());
+				if (active.text() === text || editor.hasClass('error')) {
+					return true;
+				}
+				originalContent = active.html();
+				active.text(text).trigger(evt, text);
+				active.append('<i class="edit-table fa fa-pencil visible-phone"></i>');
+
+				if (evt.result === false) {
+					active.html(originalContent);
+					active.append('<i class="edit-table fa fa-pencil visible-phone"></i>');
+				}
+			},
+			movement = function (element, keycode) {
+				if (keycode === ARROW_RIGHT) {
+					return element.next('td');
+				} else if (keycode === ARROW_LEFT) {
+					return element.prev('td');
+				} else if (keycode === ARROW_UP) {
+					return element.parent().prev().children().eq(element.index());
+				} else if (keycode === ARROW_DOWN) {
+					return element.parent().next().children().eq(element.index());
+				}
+				return [];
+			};
+		editor.blur(function () {
+			setActiveText();
+			editor.hide();
+		}).keydown(function (e) {
+			if (e.which === ENTER) {
+				setActiveText();
+				editor.hide();
+				active.focus();
+				e.preventDefault();
+				e.stopPropagation();
+			} else if (e.which === ESC) {
+				editor.val(active.text());
+				e.preventDefault();
+				e.stopPropagation();
+				editor.hide();
+				active.focus();
+			} else if (e.which === TAB) {
+				active.focus();
+			} else if (this.selectionEnd - this.selectionStart === this.value.length) {
+				var possibleMove = movement(active, e.which);
+				if (possibleMove.length > 0) {
+					possibleMove.focus();
+					e.preventDefault();
+					e.stopPropagation();
+				}
+			}
+		})
+		.on('input paste', function () {
+			var evt = $.Event('validate');
+			active.trigger(evt, editor.val());
+			if (evt.result === false) {
+				editor.addClass('error');
+			} else {
+				editor.removeClass('error');
+			}
+		});
+
+		element.on('click keypress dblclick', showEditor)
+		.css('cursor', 'pointer')
+		.keydown(function (e) {
+			var prevent = true,
+				possibleMove = movement($(e.target), e.which);
+			if (possibleMove.length > 0) {
+				possibleMove.focus();
+			} else if (e.which === ENTER) {
+				showEditor(false);
+			} else if (e.which === 17 || e.which === 91 || e.which === 93) {
+				showEditor(true);
+				prevent = false;
+			} else {
+				prevent = false;
+			}
+			if (prevent) {
+				e.stopPropagation();
+				e.preventDefault();
+			}
+		});
+
+		$('.edit-table').on('click keypress dblclick', function(){
+			console.log("clicked");
+			editor = activeOptions.editor.css('position', 'absolute').hide().appendTo(element.parent());
+
+			active = $(this).parent().prev('td');
+				if (active.length && active.attr('data-function')=="editable") {
+					editor.val(parseInt(active.text().replace(/[^0-9\.]+/g,"")))
+						.removeClass('error')
+						.show()
+						.offset(active.offset())
+						.width(active.width()+16)
+						.height(active.height()+12)
+						.css(active.css(activeOptions.cloneProperties))
+						.focus();
+						$(this).blur().parent().blur();
+					}
+		});
+
+		element.find('td').prop('tabindex', 1);
+
+		$(window).on('resize', function () {
+			if (editor.is(':visible')) {
+				editor.offset(active.offset())
+				.width(active.width())
+				.height(active.height());
+			}
+		});
+	});
+
+};
+$.fn.editableTableWidget.defaultOptions = {
+	cloneProperties: ['padding', 'padding-top', 'padding-bottom', 'padding-left', 'padding-right',
+					  'text-align', 'font', 'font-size', 'font-family', 'font-weight',
+					  'border', 'border-top', 'border-bottom', 'border-left', 'border-right'],
+	editor: $('<input class="editIp">')
+};
+
